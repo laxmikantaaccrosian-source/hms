399c85507c7158e53367c29f323aa1a9c86e4815
Laxmikanta <laxmikanta.accrosian@gmail.com>
2025-12-08 15:54:29 +0530

    another commit

--- app/Http/Livewire/DashboardAppointmentTable.php
+++ app/Http/Livewire/DashboardAppointmentTable.php
@@ -24,22 +24,28 @@ class DashboardAppointmentTable extends DataTableComponent
     }
 
     public function columns(): array
-    {
-        return [
-            Column::make(__('messages.case.patient'), 'patient.patientUser.email')
-                ->hideIf('patient.patientUser.email'),
-            Column::make(__('messages.case.patient'), 'doctor.doctorUser.email')
-                ->hideIf('doctor.doctorUser.email'),
-            Column::make(__('messages.case.patient'), 'patient.patientUser.first_name')
-                ->view('appointments.templates.columns.patient_name'),
-            Column::make(__('messages.case.doctor'), 'doctor.doctorUser.first_name')
-                ->view('appointments.templates.columns.doctor_name'),
-            Column::make(__('messages.appointment.doctor_department'), 'department.title')
-                ->view('appointments.templates.columns.department'),
-            Column::make(__('messages.appointment.date'), 'opd_date')
-                ->view('appointments.templates.columns.date'),
-        ];
-    }
+{
+    return [
+        Column::make(__('messages.case.patient'), 'patient.patientUser.email')
+            ->hideIf(fn() => true),
+
+        Column::make(__('messages.case.doctor'), 'doctor.doctorUser.email')
+            ->hideIf(fn() => true),
+
+        Column::make(__('messages.case.patient'), 'patient.patientUser.first_name')
+            ->view('appointments.templates.columns.patient_name'),
+
+        Column::make(__('messages.case.doctor'), 'doctor.doctorUser.first_name')
+            ->view('appointments.templates.columns.doctor_name'),
+
+        Column::make(__('messages.appointment.doctor_department'), 'department.title')
+            ->view('appointments.templates.columns.department'),
+
+        Column::make(__('messages.appointment.date'), 'opd_date')
+            ->view('appointments.templates.columns.date'),
+    ];
+}
+
 
     public function builder(): Builder
     {
